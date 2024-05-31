30d825429fae1f2898ee2350fd94132075f3a0e2
thecurryspice <akhil.r.baranwal@gmail.com>
2024-05-30 21:02:58 -0700

    refactor upcoming

---
+++ tapa/shared-buffer/shared-buffer-bak/src/add.cpp
@@ -0,0 +1,91 @@
+#include <tapa.h>
+#include "add.h"
+#include "brahma.h"
+
+void Mmap2Stream(tapa::mmap<const float> mmap,
+                 tapa::ostream<float>& stream) {
+  for (uint64_t i = 0; i < N; ++i) {
+    stream << mmap[i];
+  }
+}
+
+void Stream2Mmap(tapa::istream<float>& stream,
+                  tapa::mmap<float> mmap) {
+  for (uint64_t i = 0; i < N; ++i) {
+    stream >> mmap[i];
+  }
+}
+
+void vadd(tapa::istream<float>& a,
+          tapa::istream<float>& b,
+          tapa::ostream<float>& c) {
+  for (uint64_t i = 0; i < N; ++i) {
+    c << (a.read() + b.read());
+  }
+}
+
+void task1( tapa::istream<float>& vector_a,
+            tapa::ostream<sb_req_t>& tx_task1_to_sb,
+            tapa::istream<sb_rsp_t>& rx_sb_to_task1,
+            tapa::ostream<float>& tx_task1_to_task2)
+{
+  // SBIF<sb_msg_t, 2, 2> sbif1(rx_sb_to_task1, tx_task1_to_sb);
+  // sb_msg_t rsp;
+  // rsp = sbif1.grab_page(1);
+  sb_req_t request = {0};
+  request.c_dn = 1;
+  request.fields.code = SB_REQ_GRAB_PAGE;
+  request.fields.npages = 1;
+  tx_task1_to_sb << request;
+
+  // now wait for the response to be received
+  sb_rsp_t rsp;
+  rsp = rx_sb_to_task1.read();
+  // #ifndef __SYNTHESIS__
+  // printf("response message: %lu\n", (uint64_t)rsp.rsp_msg);
+  // #endif
+  for (uint64_t i = 0; i < N; ++i)
+  {
+    tx_task1_to_task2 << vector_a.read();
+  }
+}
+
+void task2( tapa::istream<float>& vector_b,
+            tapa::ostream<float>& vector_c,
+            tapa::ostream<sb_req_t>& tx_task2_to_sb,
+            tapa::istream<sb_rsp_t>& rx_sb_to_task2,
+            tapa::istream<float>& rx_task1_to_task2)
+{
+  for (uint64_t i = 0; i < N; ++i)
+  {
+    // vector_c << (float)(0);
+    vector_c << vector_b.read() + rx_task1_to_task2.read();
+    // printf("%f %f\n", vector_b.read() + rx_task1_to_task2.read());
+  }
+}
+
+////////////////
+/// WRAPPER
+////////////////
+
+void VecAdd(tapa::mmap<const float> vector_a,
+            tapa::mmap<const float> vector_b,
+            tapa::mmap<float> vector_c) {
+  tapa::stream<float> a_q("a");
+  tapa::stream<float> b_q("b");
+  tapa::stream<float> c_q("c");
+  tapa::stream<float> task1_to_task2_pageinfo ("task1_to_task2_pageinfo");
+  std::cout << "===" << std::endl;
+
+  tapa::streams<sb_req_t, SB_NXCTRS> sb_rxqs("sb_rxqs");
+  tapa::streams<sb_rsp_t, SB_NXCTRS> sb_txqs("sb_txqs");
+  
+  tapa::task()
+    .invoke(Mmap2Stream, vector_a, a_q)
+    .invoke(Mmap2Stream, vector_b, b_q)
+    .invoke(sb_task, sb_rxqs, sb_txqs)
+    // .invoke(vadd, a_q, b_q, c_q)
+    .invoke(task1, a_q,      sb_rxqs[0], sb_txqs[0], task1_to_task2_pageinfo)
+    .invoke(task2, b_q, c_q, sb_rxqs[1], sb_txqs[1], task1_to_task2_pageinfo)
+    .invoke(Stream2Mmap, c_q, vector_c);
+}
---
+++ tapa/shared-buffer/shared-buffer-class/src/sb.h
@@ -0,0 +1,95 @@
+#ifndef __SB_H__
+#define __SB_H__
+#include <stdint.h>
+#include <stddef.h>
+//#include <iostream.h>
+#include "sbif_config.h"
+#include "sbif.h"
+
+/**
+ * datatype_t --> type of the data packet (page) that will be ID-ed.
+ *                All headers and bookkeeping is appended within sharedBuffer.
+ * iports     --> Number of input ports (producers)
+ * oports     --> Number of output ports (consumers)
+ * npages     --> Total number of buffer-blocks (pages) to maintain
+ * concurrency--> Number of concurrent operations to allow.
+ *                concurrency <= (iports+oports)
+*/
+template <typename sb_msg_t,  iport_t iports, oport_t oports, section_t npages, section_t concurrency>
+class SharedBuffer {
+private:
+  // size of container (number of RAM blocks) required to store 1 `datatype_t`
+  size_t container_size;
+  // declare buffer pointer as a void type,
+  // dimension information is stored in the public variable `buffercore_t`
+  void* buffercore_p;
+  void* rxdstreams_p;
+  void* txdstreams_p;
+public:
+  // first abstract the buffercore type
+  using buffercore_t = tapa::buffer<sb_msg_t[npages], concurrency, tapa::array_partition<tapa::normal>, tapa::memcore<tapa::uram>>;
+  using rxdstreams_t = tapa::streams<sb_msg_t, iports>;
+  using txdstreams_t = tapa::streams<sb_msg_t, oports>;
+  using rxastreams_t = tapa::streams<addrtype_t, iports>;
+  using txastreams_t = tapa::streams<addrtype_t, oports>;
+
+  // constructor
+  SharedBuffer() {
+    container_size = sizeof(sb_msg_t)/BYTES_PER_RAMBLOCK;
+
+    // create temporary pointers to the buffercore and stream-arrays
+    buffercore_t* buffercore_p_ = new buffercore_t;
+    rxdstreams_t* rxdstreams_p_ = new rxdstreams_t;// rxds("rxdstreams");
+    txdstreams_t* txdstreams_p_ = new txdstreams_t;// txds("txdstreams");
+    // cast this pointer type to the corresponding private pointers
+    buffercore_p = static_cast<void*>(buffercore_p_);
+    rxdstreams_p = static_cast<void*>(rxdstreams_p_);
+    txdstreams_p = static_cast<void*>(txdstreams_p_);
+
+    // tapa::streams<datatype_t, iports> rx("rxstream");
+    // tapa::streams<datatype_t, oports> tx("txstream");
+    validate_config();
+  }
+
+  ~SharedBuffer() {
+    // delete buffercore pointer upon destruction
+    delete static_cast<buffercore_t*>(buffercore_p);
+    delete static_cast<rxdstreams_t*>(rxdstreams_p);
+    delete static_cast<txdstreams_t*>(txdstreams_p);
+  }
+
+  /**
+   * validate the configuration of the buffer. Expected to be a bunch of
+   * static assertions with some helpful messages.
+   * This function breaks compilation on an error.
+   * */ 
+  bool validate_config()
+  {
+    // print some info first
+    std::cout << "shared buff   " << std::endl;
+    std::cout << "i/p ports   : " << (unsigned)iports << std::endl;
+    std::cout << "o/p ports   : " << (unsigned)oports << std::endl;
+    std::cout << "concurrency : " << concurrency << std::endl;
+    std::cout << "typesize    : " << sizeof(sb_msg_t) << std::endl;
+    std::cout << "totalsize   : " << sizeof(buffercore_t) << std::endl;
+
+    static_assert(concurrency <= (iports + oports),\
+    "Cannot provide concurrency more than total i/o-ports");
+  }
+
+  size_t get_container_size() const {
+    return container_size;
+  }
+
+  bool connect(void* _sbif_p, sbif_depth_t _tx_depth, sbif_depth_t _rx_depth)
+  {
+    using SBIF_t = SBIF<sbif_msg_t, _tx_depth, _rx_depth>;
+    SBIF_t* sbif_p = static_cast<SBIF_t*>(_sbif_p);
+    auto temp = sbif_p->sbif_data_p->data;
+    using newType =  decltype(temp);
+    SBIF<newType, sbif_p->get_tx_depth(), sbif_p->get_rx_depth()> sbifSB1;
+    // tx_msgs = _sbif->get_rx_depth();
+  }
+};
+
+#endif // __SB_H__
