2a3ba4d650380244a4bd64f3a877870621e011ef
thecurryspice <akhil.r.baranwal@gmail.com>
2024-06-18 14:40:28 -0700

    fixed Makefile's redundant regeneration of XO object

--- tapa/shared-buffer/common.mk
+++ tapa/shared-buffer/common.mk
@@ -21,7 +21,7 @@ XRT_INI_PATH=scripts/xrt.ini
 endif
 
 ifndef MAX_SYNTH_JOBS
-MAX_SYNTH_JOBS=16
+MAX_SYNTH_JOBS=40
 endif
 
 ifndef STRATEGY
@@ -33,9 +33,9 @@ PLACEMENT_STRATEGY="EarlyBlockPlacement"
 endif
 
 KERNEL_CO=${BUILD_DIR_PREFIX}/${KERNEL}
-KERNEL_XO="${BUILD_DIR_PREFIX}/${KERNEL}.${PLATFORM}.hw.xo"
-KERNEL_XCLBIN_EM="${BUILD_DIR_PREFIX}/vitis_run_hw_emu/${KERNEL}.${PLATFORM}.hw_emu.xclbin"
-KERNEL_XCLBIN_HW="${BUILD_DIR_PREFIX}/vitis_run_hw/${KERNEL}.${PLATFORM}.hw.xclbin"
+KERNEL_XO=${BUILD_DIR_PREFIX}/${KERNEL}.${PLATFORM}.hw.xo
+KERNEL_XCLBIN_EM=${BUILD_DIR_PREFIX}/vitis_run_hw_emu/${KERNEL}.${PLATFORM}.hw_emu.xclbin
+KERNEL_XCLBIN_HW=${BUILD_DIR_PREFIX}/vitis_run_hw/${KERNEL}.${PLATFORM}.hw.xclbin
 RUNXOVDBG_OUTPUT_DIR="${BUILD_DIR_PREFIX}/vitis_run_hw_emu"
 VPP_TEMP_DIR="${RUNXOVDBG_OUTPUT_DIR}/${KERNEL_TOP}_${PLATFORM}.temp"
 
@@ -66,13 +66,13 @@ ${KERNEL_XO}: ${KERNEL_CO}
 	@echo "[MAKE]: Compiling for XO target"
 	tapac -vv -o ${KERNEL_XO} src/${KERNEL}.cpp --platform ${PLATFORM} --top ${KERNEL_TOP} --work-dir ${KERNEL_XO}.tapa --enable-buffer-support --connectivity connectivity.ini --max-parallel-synth-jobs 24 --separate-complex-buffer-tasks
 
-runxo: ${KERNEL_XO}
+runxo: xo
 	@echo "[MAKE]: Target HW_EMU"
 	@echo "[MAKE]: Running HW_EMU (.xo)"
 	@cd ${BUILD_DIR_PREFIX}
 	${KERNEL_CO} ${KERNEL_ARGS} --bitstream=${KERNEL_XO}
 
-runxodbg: ${KERNEL_XO}
+runxodbg: xo
 	@echo "[MAKE]: Target waveform"
 	@echo "[MAKE]: Running waveform for HW_EMU (.xo)"
 	@cd ${BUILD_DIR_PREFIX}
@@ -81,7 +81,7 @@ runxodbg: ${KERNEL_XO}
 	@echo "open_wave_config {${BUILD_DIR_PREFIX}/wave.wcfg}" >> ${BUILD_DIR_PREFIX}/xosim/output/run/run_cosim_no_exit.tcl
 	vivado -mode gui -source ${BUILD_DIR_PREFIX}/xosim/output/run/run_cosim_no_exit.tcl
 
-runxov: ${KERNEL_XO}
+runxov: xo
 	@echo "[MAKE]: Target HW_EMU"
 	@echo "[MAKE]: Building .xclbin through Vitis"
 	@cd ${BUILD_DIR_PREFIX}
@@ -98,7 +98,7 @@ runxov: ${KERNEL_XO}
 	@cd ${BUILD_DIR_PREFIX}
 	${KERNEL_CO} ${KERNEL_ARGS} --bitstream=${KERNEL_XCLBIN_EM}
 
-runxovdbg: ${KERNEL_XO}
+runxovdbg: xo
 	@echo "[MAKE]: Target HW_EMU"
 	@echo "[MAKE]: Building .xclbin through Vitis"
 	export XRT_INI_PATH=${PWD}/scripts/xrt.ini
